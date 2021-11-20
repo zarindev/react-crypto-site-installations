af71028bb354c7e10f365f2b2c0f90ac3cae3c73
Md Zarin <mdzarinjerry@gmail.com>
2021-11-21 03:51:51 +0600

    hudai

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -6,12 +6,13 @@ import { Link } from 'react-router-dom';
 
 
 import { useGetCryptosQuery } from '../Services/CryptoApi';
+import { Cryptocurrencies, News } from '.';
 
 const {Title} = Typography;
 
 const Homepage = () => {
 
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(10);
 
     const globalStats = data?.data?.stats;
 
@@ -31,8 +32,15 @@ const Homepage = () => {
             <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
         </Row>
         <div className="home-heading-container">
-            <Title level={2} className="home-title">Top 10 Cryptocurrencies</Title>
+            <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+            <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
         </div>
+        <Cryptocurrencies simplified/>
+        <div className="home-heading-container">
+            <Title level={2} className="home-title">Latest Crypto News</Title>
+            <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+        </div>
+        <News simplified/>
         </>
     )
 }
